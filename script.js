17a21cf4d2df205596407431dc69e80bee5699d5
Saadniz <164231292+Saadnizz@users.noreply.github.com>
2025-11-20 10:29:08 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+const user = [
+  {
+    id: 1,
+    name: "Saad",
+    post: "I have completed my frontend course",
+    Comments: ["good job", "Mubarakoon"],
+  },
+  {
+    id: 2,
+    name: "Darakhshan",
+    post: "fronted expert",
+    Comment: ["Thoughtfull", "helpfull"],
+  },
+  {
+    id: 3,
+    name: "Rayyan",
+    post: "Today is our session # 29 error handling",
+    Comment: "hardworking",
+  },
+];
+
+
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = user;
+
+      if (data) {
+        resolve(data);
+      } else {
+        reject("User array not found");
+      }
+    }, 3000);
+  });
+};
+
+
+const fetchdatabyId = (recData) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = recData.find((user) => {
+        return user.name === "Darakhshan";
+      });
+
+      if (person) {
+        resolve(person);
+      } else {
+        reject("User not found");
+      }
+    }, 2000);
+  });
+};
+
+console.log("Fetching data...");
+
+fetchData()
+  .then(fetchdatabyId)
+  .then((p) => {
+    let user_name = p.name;
+    console.log(user_name.toLowerCase());
+  })
+  .catch((err) => {
+    console.log("Error: " + err);
+  });
